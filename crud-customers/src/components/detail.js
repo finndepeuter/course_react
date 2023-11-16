55166c0d7893c557a4e76faf0e63ae07d109b44a
Finn De Peuter <r0831240@student.thomasmore.be>
2023-11-16 16:56:11 +0100

    added crud customers

---
+++ crud-customers/src/components/detail.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import MockApi from '../apis/mock_api';
+
+// import MockApi from '../apis/mock_api';
+import { useNavigate} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+export default function Detail({onClick }) {
+  const { id } = useParams();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [iAgree, setIAgree] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    try {
+      const customer = {
+        id,
+        firstName,
+        lastName,
+        iAgree,
+      };
+
+      if (id !== '0') {
+        await MockApi.put(customer);
+      } else {
+        await MockApi.post(customer);
+      }
+    
+      navigate(-1);
+    } catch (error) {
+      console.error("error editing customer")
+    }
+  }
+
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(id);
+      if (id !== '0') {
+      const result = await MockApi.getbyid(id);
+      const customer = result.data;
+      setFirstName(customer.firstName);
+      setLastName(customer.lastName);
+      setIAgree(customer.iAgree);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+
+  return (
+    <div>
+      <Form className="create-form">
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='I agree to the Terms and Conditions' checked={iAgree} onChange={() => setIAgree(!iAgree)}/>
+        </Form.Field>
+        <Button type='button' onClick={handleSubmit}>
+          Submit
+          </Button>
+      </Form>
+    </div>
+  )
+}
---
+++ crud-customers/src/components/list.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { Table, Button } from 'semantic-ui-react';
+import { Link, useParams } from 'react-router-dom';
+import MockApi from '../apis/mock_api';
+
+export default function List() {
+  const [customers, setCustomers] = useState([]);
+
+  const getCustomers = async () => {
+    const result = await MockApi.get();
+    setCustomers(result.data);
+  }
+
+  useEffect(() => {
+    getCustomers()
+  }, []);
+
+  const deleteCustomer = async (id) => {
+    await MockApi.delete(id);
+    setCustomers(customers.filter(
+      customer => customer.id !== id
+    ));
+  }
+
+  return (
+    <div>
+      <Link to='/create/0'><Button>Create</Button></Link>
+      <Table singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>I Agree</Table.HeaderCell>
+            <Table.HeaderCell>Update</Table.HeaderCell>
+            <Table.HeaderCell>Delete</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {customers.map((customer) => {
+            return (
+              <Table.Row key={customer.id}>
+                <Table.Cell>{customer.firstName}</Table.Cell>
+                <Table.Cell>{customer.lastName}</Table.Cell>
+                <Table.Cell>{customer.iAgree ? 'Checked' : 'Unchecked'}</Table.Cell>
+                <Table.Cell><Link to={'/update/' + customer.id}><Button>Edit</Button></Link></Table.Cell>
+                <Table.Cell>
+                  <Button onClick={() => deleteCustomer(customer.id)}>Delete</Button>
+                </Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  )
+}
